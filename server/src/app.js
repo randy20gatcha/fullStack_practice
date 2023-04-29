3aef6c57aa55191f4a6d99b5d9ba39ce6b37f496
Randy <randy.gatchalian@medisureonline.com>
2023-04-29 09:53:39 +0800

    express app build

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,26 @@
-console.log("hello");
+// Build a basic express server
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+// Build an express app, building REST endpoints
+const app = express();
+app.use(morgan("combined"));
+app.use(bodyParser.json()); // parse any json request sent in
+app.use(cors()); // possible security issues source
+
+/*
+    get
+    post
+    put
+    patch
+    delete
+*/
+app.get("/status", (req, res) => {
+    res.send({
+        message: "hello Randy."
+    });
+});
+
+app.listen(process.env.PORT || 8081); // environment variable
